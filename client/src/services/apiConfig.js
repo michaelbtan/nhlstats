dca0b37f0f1164a339abb2091744ab6df6832525
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-06-14 15:01:55 -0400

    setup second endpoint

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 
-const teams_baseURL = `https://statsapi.web.nhl.com/api/v1/teams`;
-// const teamStats_baseURL = `https://statsapi.web.nhl.com/api/v1/teams/${id}/stats`;
+const api = `https://statsapi.web.nhl.com/api/v1/teams`;
+// const teamData_baseURL = `https://statsapi.web.nhl.com/api/v1/teams/${id}/stats`;
 
-// imports the teams data from the api
+// export the teams data from the api
 export const getTeamsData = async () => {
   try {
-    const res = await axios.get(teams_baseURL);
+    const res = await axios.get(api);
     return res.data.teams; //filters to the team objects
   } catch (error) {
+    throw error;
+  }
+};
+
+// export the individual team data from the api
+export const getIndividualTeamData = async (id) => {
+  try {
+    const res = await axios.get(api, `/${id}/stats`)
+    return res
+  } catch(error) {
     throw error;
   }
 };
