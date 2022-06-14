fa7d5d94ab0140fc464f6de1beb87465acbfdf6e
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-06-14 17:19:42 -0400

    fixed the venue city and location in the team card

--- client/src/components/TeamCard.js
+++ client/src/components/TeamCard.js
@@ -6,7 +6,12 @@ export default function TeamCard({ team, id }) {
     <div id={id}>
       <img src="https://i.imgur.com/0mllQct.png" width="100px" alt="NHL logo"/>
       <h2>{team.franchise.teamName}</h2>
-      <h4>{ team.venue.city }, {team.locationName }</h4>
+      {/* checks if the venue city and location name is the same */}
+      {
+        team.venue.city === team.locationName ?
+          <h4>{team.locationName}</h4>
+          : <h4>{team.venue.city}, {team.locationName}</h4>
+      }
       <h4>Conference: {team.conference.name}</h4>
       <h4>Division: {team.division.nameShort}</h4>
       <Link to={`/${team.id}`}>See more</Link>
