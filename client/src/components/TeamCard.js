913f66ba6f660b6472e99e46cc2486821121bf19
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-06-14 13:33:17 -0400

    started team card and team names rendering

---
+++ client/src/components/TeamCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TeamCard({ team }) {
+  return (
+    <div>
+      <h1>{team.name}</h1>
+    </div>
+  )
+}
--- client/src/screens/TeamDirectory.js
+++ client/src/screens/TeamDirectory.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import TeamCard from '../components/TeamCard';
 import { getTeamsData } from '../services/apiConfig';
 
 export default function TeamDirectory() {
 
+  //Holds the array of team objects
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
     const getTeams = async () => {
       const teams = await getTeamsData();
       setTeams(teams);
-      console.log(teams);
+      console.log(teams)
     }
-    getTeams()
+    getTeams();
   }, [])
 
   return (
     <div>
-
+      {
+        teams.map((team) => (
+          <TeamCard team={team} />
+        ))
+      }
     </div>
   )
 }
