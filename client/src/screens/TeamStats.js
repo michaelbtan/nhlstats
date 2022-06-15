52c22cdf8c7e9a675d974d2442ee1ccd8b3022ed
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-06-15 13:26:19 -0400

    fixed team data rendering

--- client/src/screens/TeamStats.js
+++ client/src/screens/TeamStats.js
@@ -12,55 +12,54 @@ export default function TeamInfo() {
   const { id } = useParams(); //id of the team that is selected
   const [mock, setMock] = useState({})
   const [mock2, setMock2] = useState({})
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     try {
       const getTeamData = async () => {
         const team = await getTeamStats(id);
         setTeamStats(team);
-        setMock(mockData?.[0].splits?.[0].stat);
-        setMock2(mockData?.[1].splits?.[0].stat);
-        console.log('fffff', Object.keys(mock))
-        console.log('fffff', typeof Object.keys(mock))
+        setMock(teamStats?.[0].splits?.[0].stat);
+        setMock2(teamStats?.[1].splits?.[0].stat);
+        setIsFetching(false);
       }
       getTeamData();
     } catch (error) {
       throw error;
     }
-  }, [id]);
+  }, [id, teamStats]);
 
   return (
     <div>
+      {
+        isFetching ? <p>Loading...</p> :
+      <>
       <div className="teamStats_header">
         <img src="https://i.imgur.com/0mllQct.png" width="100px" alt="NHL logo"/>
-        <h2>{mockData?.[0].splits?.[0].team.name}</h2>
+        <h2>{teamStats?.[0].splits?.[0].team.name}</h2>
       </div>
       <div className="teamStats">
         <table>
+          <thead>
           <tr>
-            {
-              Object.keys(mock).map((key) => (
-                <th>{key}</th>
-              ))
-            }
+            {Object.keys(mock).map((key, id) => (
+                <th key={id}>{key}</th>))}
+          </tr>
+          </thead>
+          <tbody>          
+          <tr>
+            {Object.values(mock).map((key, id ) => (
+                <td key={id}>{key}</td>))}
           </tr>
           <tr>
-            {
-              Object.values(mock).map((key) => (
-                <th>{key}</th>
-              ))
-            }
+            {Object.values(mock2).map((key, id) => (
+                <td key={id}>{parseInt(key.slice(0, -2))}</td>))}
           </tr>
-          {/* <tr>
-            {
-              Object.values(mock2).map((key) => (
-                <th>{key}</th>
-              ))
-            }
-          </tr> */}
+          </tbody>
         </table>
-        <h5>{JSON.stringify(mock2)}</h5>
       </div>
+      </>
+      }
       <BackBtn />
     </div>
   )
