474df12f5b38b693ee2347089cadf41bf5ae82e2
Michael Tan <michaeltan@MacBook-Pro-2.fios-router.home>
2022-06-15 17:39:16 -0400

    added ranking colors

--- client/src/screens/TeamStats.js
+++ client/src/screens/TeamStats.js
@@ -8,9 +8,13 @@ import { getTeamStats } from '../services/apiConfig';
 
 export default function TeamInfo() {
 
+  //queried team data from the api
   const [teamStats, setTeamStats] = useState({});
-  const { id } = useParams(); //id of the team that is selected
+  //id of the team that is selected
+  const { id } = useParams(); 
+  //stats for the season
   const [stats, setStats] = useState({})
+  //ranking vs the rest of the league
   const [rank, setRank] = useState({})
   const [isFetching, setIsFetching] = useState(true);
 
@@ -19,7 +23,9 @@ export default function TeamInfo() {
       const getTeamData = async () => {
         const team = await getTeamStats(id);
         setTeamStats(team);
+        // queries season stats for the team
         setStats(teamStats?.[0].splits?.[0].stat);
+        //queries to season rankings
         setRank(teamStats?.[1].splits?.[0].stat);
         setIsFetching(false);
       }
@@ -32,36 +38,50 @@ export default function TeamInfo() {
   return (
     <div>
       {
+        /* Checking to see if the queries team data has loaded */
         isFetching ? <p>Loading...</p> :
       <>
       <div className="teamStats_header">
         <img src="https://i.imgur.com/0mllQct.png" width="100px" alt="NHL logo"/>
         <h2>{teamStats?.[0].splits?.[0].team.name}</h2>
         <h3>2021-2022 Stats</h3>
+        <BackBtn />
       </div>
       <div className="teamStats">
         <table>
           <thead>
-          <tr>
-            {Object.keys(stats).map((key, id) => (
-                <th key={id}>{key}</th>))}
-          </tr>
+            <tr>
+              {Object.keys(stats).map((key, id) => (
+                  <th key={id}>{key}</th>))}
+            </tr>
           </thead>
           <tbody>          
-          <tr>
-            {Object.values(stats).map((value, id ) => (
-                <td key={id}>{value}</td>))}
-          </tr>
-          <tr>
-            {Object.values(rank).map((key, id) => (
-                <td key={id}>{parseInt(key.slice(0, -2))}</td>))}
-          </tr>
+            <tr>
+              {Object.values(stats).map((value, id ) => (
+                  <td key={id}>{value}</td>))}
+            </tr>
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(rank).map((key, id) => (
+                  <th key={id}>{key}</th>))}
+            </tr>
+          </thead>
+          <tbody>          
+            <tr>
+              {
+              Object.values(rank).map((key, id) => (
+                <td key={id} className={parseInt(key.slice(0, -2)) >= 16 ? "below" : "above"}
+                >{key}</td>))
+              }
+            </tr>
           </tbody>
         </table>
       </div>
       </>
       }
-      <BackBtn className="backBtn"/>
     </div>
   )
 }
